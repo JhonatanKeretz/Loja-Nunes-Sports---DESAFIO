6a19ec2d666c307f018045df415a20790664759d
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2024-01-08 20:57:32 -0300

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Lista from '../views/Lista.vue'
+import Form from '../views/Formulario.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'list',
+    component: Lista
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/form',
+    name: 'form',
+    component: Form
   }
+  
 ]
 
 const router = new VueRouter({
